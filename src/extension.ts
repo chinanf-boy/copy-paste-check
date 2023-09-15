b68c8ceccc650a7e40ba25c6bac8ead26d00da25
savissimo <simone.saviolo@gmail.com>
2023-09-15 19:45:47 +0200

    Added a link to the duplicated content in the hover message

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,14 @@ import * as vscode from 'vscode';
 import { DuplicatedCode } from './duplicated-code';
 import { DuplicatedCodeProvider } from './duplicated-code.provider';
 import { IClone } from 'jscpd';
+import path from 'path';
+
+interface IOpenTargetFileArguments {
+  uri: string, 
+  preview: boolean,
+  selection?: vscode.Selection,
+  justPlaceCursor?: boolean,
+}
 
 const duplicatedCodeDecorationType = vscode.window.createTextEditorDecorationType({
   borderWidth: '0',
@@ -36,13 +44,33 @@ function createDecorationsOnTextEditor(duplicatedCodeProvider: DuplicatedCodePro
   
   textEditor?.setDecorations(duplicatedCodeDecorationType, clonesInThisFile.map(c => {
     const duplication = { a: c.duplicationA, b: c.duplicationB }[c.side!];
-    const duplicationOtherSide = { a: c.duplicationB, b: c.duplicationA }[c.side!];
+    const duplicationOtherSide = { a: c.duplicationB, b: c.duplicationA }[c.side!]!;
+    const otherSidePath = path.relative(fileName, duplicationOtherSide.sourceId);
+    const duplicationOtherSideStart = duplicationOtherSide.start;
+    const duplicationOtherSideStartString = `${duplicationOtherSideStart.line}:${duplicationOtherSideStart.column ?? 1}`;
+    
+    const args: IOpenTargetFileArguments = {
+      uri: duplicationOtherSide.sourceId,
+      preview: true,
+      selection: new vscode.Selection(
+        duplicationOtherSide.start.line - 1, (duplicationOtherSide.start.column ?? 1) - 1,
+        duplicationOtherSide.end.line - 1, (duplicationOtherSide.end.column ?? 1) - 1
+      ),
+      justPlaceCursor: true,
+    };
+    const openCommandUri = vscode.Uri.parse(
+      `command:duplicatedCode.openTargetFile?${encodeURIComponent(JSON.stringify(args))}`
+    );
+
+    const hoverMessageContent = new vscode.MarkdownString(`Duplicated code found in **${otherSidePath}**:${duplicationOtherSideStartString} ([review](${openCommandUri})):\n`
+    + '```\n' + duplicationOtherSide?.fragment + '\n```');
+    hoverMessageContent.isTrusted = true;
+
     return { 
       range: new vscode.Range(
         duplication!.start.line - 1, (duplication!.start.column ?? 1) - 1, 
         duplication!.end.line - 1, (duplication!.end.column ?? 1) - 1),
-        hoverMessage: `Duplicated code found in ${duplicationOtherSide?.sourceId}:${duplicationOtherSide?.start.line}:${duplicationOtherSide?.start.column ?? 1}:\n`
-          + '```\n' + duplicationOtherSide?.fragment + '\n```', 
+        hoverMessage: hoverMessageContent, 
       };
     }));
 }
@@ -58,6 +86,15 @@ export function activate() {
 
   vscode.commands.registerCommand('duplicatedCode.openFile', (duplicateCode: DuplicatedCode) => {
     duplicateCode.openFile();
+  });
+
+  vscode.commands.registerCommand('duplicatedCode.openTargetFile', async (options: IOpenTargetFileArguments) => {
+    const doc = await vscode.workspace.openTextDocument(options.uri);
+    const newEditor = await vscode.window.showTextDocument(doc, { preview: options.preview });
+    if (options.selection) {
+      newEditor.selections = [ new vscode.Selection(options.selection.start, (options.justPlaceCursor ?? false) ? options.selection.start : options.selection.end) ];
+      newEditor.revealRange(options.selection, vscode.TextEditorRevealType.InCenter);
+    }
   });
 
   vscode.window.onDidChangeActiveTextEditor((e) => createDecorationsOnTextEditor(duplicatedCodeProvider, e));
